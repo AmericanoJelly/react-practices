59c1ae03371e93db49a0cb095fa61b5a5c8f2f66
YUJIN <yujinj704@gmail.com>
2022-07-14 17:18:32 +0900

    JSX tutorial

---
+++ 1.basics/ex08/src/04/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content() {
+    // return (
+    //     <div>
+    //         <h2>Content</h2>
+    //     </div>
+    // );
+    return React.createElement('p', null, 'Pure React(React API)로 컴포넌트 만들기');
+}
+
+export default Content;
---
+++ 1.basics/ex08/src/04/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header(props) {
+    return React.createElement('h2', null, 'App04')
+}
+
+export default Header;
