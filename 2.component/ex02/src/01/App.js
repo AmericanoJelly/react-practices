f9a840b99e5c1152da936d4c2f5213d3edf65f69
yujin <yujin@yujinui-MacBookAir.local>
2022-07-15 15:18:43 +0900

    02

---
+++ 2.component/ex02/src/01/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Component() {
+    const h1Style = {
+
+    };
+    render() {
+        return (
+            <div id='App'>
+                <h1>Inline Styling</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
